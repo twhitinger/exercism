582c819be201235c885cf2aed226c7831a2a3876
Trent Whitinger <twhitinger@gmail.com>
2016-11-16 14:00:16 -0600

    bob done done it

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,9 +1,17 @@
-'use strict'
+'use strict';
 
 class Bob {
 
   hey(input){
-
+   if (input === input.toUpperCase() && input.search(/[A-z]/) > -1) {
+      return "Whoa, chill out!";
+    } else if ((input[input.length - 1] === "?")) {
+      return "Sure.";
+    } else if (input.replace(/\s*/, '').length === 0) {
+      return "Fine. Be that way!";
+    } else {
+      return "Whatever.";
+    }
   }
 }
 

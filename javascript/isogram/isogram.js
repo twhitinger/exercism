3ea81adf6c52607877542ac8df096db60fff2354
Trent Whitinger <twhitinger@gmail.com>
2016-11-21 14:48:13 -0600

    solution

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,8 +1,22 @@
 "use strict";
 
 class Isogram {
+  constructor (word) {
+    this.isogram = word;
+  }
 
   isIsogram() {
-
+    let isogram = this.isogram.toLowerCase().replace(/[-\s]/g,'');
+    const acc = Object.create(null);
+    for (let letter of isogram) {
+      if(acc[letter]) {
+        return false;
+      } else {
+        acc[letter] = letter;
+      }
+    }
+    return true;
   }
 }
+
+module.exports = Isogram;

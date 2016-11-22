0bc745b0bedeee8c3797fb0203592849c001b388
Trent Whitinger <twhitinger@gmail.com>
2016-11-22 01:13:25 -0600

    things

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,53 @@
+'use strict';
+
+class Anagram {
+  constructor(input) {
+    this.original = input.toLowerCase();
+    this.count = input.length;
+    this.input = this.table(input);
+
+  }
+
+  table(input) {
+    let word = input.toLowerCase();
+    let acc = Object.create(null);
+    for (let letter of word){
+
+      if ( acc[letter] ){
+        acc[letter]++;
+      } else {
+        acc[letter] = 1;
+      }
+    }
+    return acc;
+  }
+
+  matches(arr, ...strings) {
+    if(strings){
+      strings.push(arr);
+      arr = strings;
+    }
+    let matches = [];
+    for(let word of arr) {
+      if(word.toLowerCase() === this.original) {
+        return [];
+      }
+      let acc = this.table(word);
+      let counter = 0;
+
+      for(let letter of word) {
+        if (this.input[letter] !== acc[letter] ) {
+          counter = 0;
+        } else if (acc[letter] === this.input[letter]) {
+          counter++;
+        }
+      }
+      if (counter === this.count){
+        matches.push(word);
+      }
+    }
+    return matches;
+  }
+}
+
+module.exports = Anagram;

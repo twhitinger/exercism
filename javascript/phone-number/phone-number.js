5313f11a7cfc10bdff9642714e473dc061f22d4e
Trent Whitinger <twhitinger@gmail.com>
2016-11-21 22:19:39 -0600

    phone number give me your phone number

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -6,9 +6,25 @@ class PhoneNumber {
     this.seq = seq;
   }
 
-  number() {
+  areaCode() {
+    return this.seq.slice(0,3);
+  }
 
+  toString() {
+    return `(${this.areaCode()}) ${this.seq.slice(3,6)}-${this.seq.slice(6,10)}`;
+  }
+
+  number() {
+    let number = this.seq.replace(/\D/g,'');
+    if (number.length === 10 ){
+      return number;
+    } else if (number.length === 11 && number[0] === '1') {
+      return number.slice(1,11);
+    } else {
+      return '0000000000';
+    }
   }
 }
+
 
 module.exports = PhoneNumber;

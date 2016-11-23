6e91552e8a94c387906fbbf333f61dfd905bdd4d
Trent Whitinger <twhitinger@gmail.com>
2016-11-22 18:30:03 -0600

    school list

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -8,6 +8,23 @@ class School {
   roster() {
     return this.schoolRoster;
   }
+
+  add(name, grade) {
+    if (this.schoolRoster[grade]) {
+      this.schoolRoster[grade].push(name);
+      this.schoolRoster[grade] = this.schoolRoster[grade].sort();
+    } else {
+      this.schoolRoster[grade] = [name];
+    }
+  }
+
+  grade(grade) {
+    if (this.schoolRoster[grade]) {
+      return this.schoolRoster[grade];
+    } else {
+      return [];
+    }
+  }
 }
 
 module.exports = School;

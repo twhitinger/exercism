016af1a59034b136a07f3e74b23438cef7382bb1
Trent Whitinger <twhitinger@gmail.com>
2016-11-23 14:01:11 -0600

    tried a few different random generators not getting to the 10000 uniques in 9900+ range

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,10 +1,45 @@
 'use strict';
-
+let taken = {};
 
 class Robot {
+  constructor() {
+    this.name = this.generateRandomName();
+  }
 
-  name(){
+  reset() {
+    this.name = this.generateRandomName();
+  }
 
+  generateRandomName() {
+    let checkName = `${this.genLetters()}${this.genNumbers()}`;
+    if (taken[checkName]) {
+      this.generateRandomName();
+    } else {
+      taken[checkName] = checkName;
+      return checkName;
+    }
+  }
+
+  genLetters() {
+    const chars = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
+    const string_length = 2;
+    let randomstring = '';
+    for (let i=0; i < string_length; i++) {
+      let rnum = chars.charAt(Math.floor(Math.random() * chars.length));
+      randomstring += rnum;
+    }
+    return randomstring;
+  }
+
+  genNumbers() {
+    const chars = "0123456789";
+    const string_length = 3;
+    let randomstring = '';
+    for (let i=0; i < string_length; i++) {
+      let rnum = chars.charAt(Math.floor(Math.random() * chars.length));
+      randomstring += rnum;
+    }
+    return randomstring;
   }
 }
 
